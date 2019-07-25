a2ce93b1d5bc1d85b592ef45d6e8fef16cace159
Mahmud <mahmud.siraj@outlook.com>
2019-07-25 15:57:10 +0100

    Initial project add

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType petType;
+    private Owner owner;
+    private LocalDate birthDat;
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDat() {
+        return birthDat;
+    }
+
+    public void setBirthDat(LocalDate birthDat) {
+        this.birthDat = birthDat;
+    }
+}
