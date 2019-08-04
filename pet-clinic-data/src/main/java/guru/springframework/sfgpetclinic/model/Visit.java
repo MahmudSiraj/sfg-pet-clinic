227542ff3efd235441729697b63b1d2f4da37e93
Mahmud <mahmud.siraj@outlook.com>
2019-08-05 00:03:09 +0100

    ADDED CONTACT INFO PROPERTIES Closes #26

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -5,9 +5,7 @@ import java.time.LocalDate;
 public class Visit extends BaseEntity {
 
     private LocalDate date;
-
     private String description;
-
     private Pet pet;
 
     public LocalDate getDate() {
