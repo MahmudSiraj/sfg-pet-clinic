06a313736cae2fb36c672f4d8f49dcf074b81f31
Mahmud <mahmud.siraj@outlook.com>
2019-08-05 00:24:54 +0100

    Closes #30

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -3,4 +3,12 @@ package guru.springframework.sfgpetclinic.model;
 public class PetType extends BaseEntity {
 
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
